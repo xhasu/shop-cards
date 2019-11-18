28e1ac884fd9cd6d01fff37a2bec72e894e5cf2d
Suarez <HECTOR.SUAREZ@yrbrands.com>
2019-11-18 11:27:29 -0500

    update images, responsive and scroll, add animation delay

--- js/ui.js
+++ js/ui.js
@@ -14,11 +14,11 @@
 		var vm = this;
 
 		vm.data = [
-			{'id': 1, 'category': 'Nike', 'type': 'Air Jordan 13', 'media': 'img/air-jordan13-retro.webp'},
-			{'id': 2, 'category': 'Nike', 'type': 'Air Jordan 5', 'media': 'img/air-jordan5-retro.webp'},
-			{'id': 3, 'category': 'Nike', 'type': 'Jordan Basketball', 'media': 'img/jordan-basketball.webp'},
-			{'id': 4, 'category': 'Nike', 'type': 'Kyrie 6 Basketball', 'media': 'img/kyrie6-basketball.webp'},
-			{'id': 5, 'category': 'Nike', 'type': 'Zoom Moc', 'media': 'img/zoom-moc.webp'},
+			{'id': 1, 'category': 'Nike', 'type': 'Air Jordan 13', 'media': 'img/air-jordan13-retro.jpg'},
+			{'id': 3, 'category': 'Nike', 'type': 'Air Jordan 11', 'media': 'img/air-jordan11-retro.jpg'},
+			{'id': 2, 'category': 'Nike', 'type': 'Air Jordan 5', 'media': 'img/air-jordan5-retro.jpg'},
+			{'id': 4, 'category': 'Nike', 'type': 'Kyrie 6 Basketball', 'media': 'img/kyrie6-basketball.jpg'},
+			{'id': 5, 'category': 'Nike', 'type': 'Zoom Moc', 'media': 'img/zoom-moc.jpg'},
 		];
 
 	};
